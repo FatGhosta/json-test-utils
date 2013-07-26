2b68e0d60b6ea8ddd36a148c0e4f1bc7fcca1a86
张岊 <milo_zhang_bs@hotmail.com>
2013-07-26 18:05:50 +0800

    Create jsonwatchjob.sh

---
+++ jsonwatchjob.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+#config json url list
+jsonUrlList=(
+'http://example.com/example.do'
+)
+countUrl=${#jsonUrlList[@]}
+
+#config mail list
+watchEmail=(
+'zackzhang.g@google.com'
+)
+countEmail=${#watchEmail[@]}
+
+i=0
+while [ "$i" -lt "$countUrl" ]
+do
+	  result=`curl ${jsonUrlList[$i]} | ./JSON.sh 2>&1 | grep "EXPECTED"`
+    if [ -n "$result" ]
+	  then
+		    j=0
+		    while [ "$j" -lt "$countEmail" ]
+		    do
+            echo ${jsonUrlList[$i]}"格式错误，不可用" | mail -s "json错误报告" ${watchEmail[$j]}
+			      let "j = $j + 1"
+		    done
+	  fi
+    let "i = $i + 1"
+done
